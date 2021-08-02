5c4a41b1a26ab6fbadb5f489e096c3da9f7d9b8e
Zilin Gao <46180928+ZilinGao@users.noreply.github.com>
2021-08-02 20:55:59 +0800

    dataset name modified

--- script/Mini_K200/test_K200_TCP_TSN_R50_8f.sh
+++ script/Mini_K200/test_K200_TCP_TSN_R50_8f.sh
@@ -1,5 +1,5 @@
 python test_models.py \ 
-    kinetics --arch resnet50 \
+    mini_kinetics_200 --arch resnet50 \
     --test_crops 3 --ten_clips_sample \
     --test_segments 8 \
     --full_res --input_size 256 \
