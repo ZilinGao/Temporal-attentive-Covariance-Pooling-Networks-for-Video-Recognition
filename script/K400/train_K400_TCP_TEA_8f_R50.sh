9eaa563ef86733b7ae00a1fab2ae315b01025198
Zilin Gao <46180928+ZilinGao@users.noreply.github.com>
2021-08-04 17:48:07 +0800

    lr

--- script/K400/train_K400_TCP_TEA_8f_R50.sh
+++ script/K400/train_K400_TCP_TEA_8f_R50.sh
@@ -1,8 +1,8 @@
 python main.py \
     kinetics RGB --arch tea50 \
     --num_segments 8 --gpus 0 1 2 3 --gd 20\
-    --lr 0.01 --lr_steps 20 40 --epochs 50 \
-    --batch-size 64  -j 8 --dropout 0.5 \
+    --lr 0.01 --lr_steps 20 30 --epochs 40 \
+    --batch-size 32  -j 8 --dropout 0.5 \
     --consensus_type=avg --eval-freq=1 \
     --shift --shift_div=8 --shift_place=blockres \
     --npb --wd 1e-4\
